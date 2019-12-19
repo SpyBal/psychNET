ef74f66ee9c41df5b91abf0a9d8db6898e0ecdaf
Spyros Balafas <s.balafas@rug.nl>
2019-12-19 13:40:02 +0000

    version 0.0.3

--- R/impute_psychovar.R
+++ R/impute_psychovar.R
@@ -28,79 +28,79 @@ impute_psychovar <- function(res, method="Kalman.struct"){
       method <- as.character(label(z))
       
       if (method=="Kalman.arima"){
-        z <- as.numeric(na.kalman(z,model = "auto.arima"))
+        z <- as.numeric(na_kalman(z,model = "auto.arima"))
       }
       if (method=="Kalman.struct"){
-        z <- as.numeric(na.kalman(z,model = "StructTS"))
+        z <- as.numeric(na_kalman(z,model = "StructTS"))
       }
       if (method=="Interpol.linear"){
-        z <- as.numeric(na.interpolation(z, option = "linear"))
+        z <- as.numeric(na_interpolation(z, option = "linear"))
       }
       if (method=="Interpol.spline"){
-        z <- as.numeric(na.interpolation(z, option = "spline"))
+        z <- as.numeric(na_interpolation(z, option = "spline"))
       }
       if (method=="Interpol.stine"){
-        z <- as.numeric(na.interpolation(z, option = "stine"))
+        z <- as.numeric(na_interpolation(z, option = "stine"))
       }
       if (method=="LOCF"){
-        z <- as.numeric(na.locf(z, option = "locf", na.remaining = "rev"))
+        z <- as.numeric(na_locf(z, option = "locf", na_remaining = "rev"))
       }
       if (method=="NOCB"){
-        z <- as.numeric(imputeTS::na.locf(z, option = "nocb", na.remaining = "rev"))
+        z <- as.numeric(na_locf(z, option = "nocb", na_remaining = "rev"))
       }
       if (method=="MA.simple"){
-        z <- as.numeric(imputeTS::na.ma(z, k=4, weighting = "simple"))
+        z <- as.numeric(na_ma(z, k=4, weighting = "simple"))
       }
       if (method=="MA.linear"){
-        z <- as.numeric(imputeTS::na.ma(z, k=4, weighting = "linear"))
+        z <- as.numeric(na_ma(z, k=4, weighting = "linear"))
       }
       if (method=="MA.exponent"){
-        z <- as.numeric(imputeTS::na.ma(z, k=4, weighting = "exponential"))
+        z <- as.numeric(na_ma(z, k=4, weighting = "exponential"))
       }
       if (method=="mean"){
-        z <- as.numeric( imputeTS::na.mean(z, option = "mean"))
+        z <- as.numeric(na_mean(z, option = "mean"))
       }
       if (method=="mode"){
-        z <- as.numeric(imputeTS::na.mean(z, option = "mode"))
+        z <- as.numeric(na_mean(z, option = "mode"))
       }
       if (method=="median"){
-        z <- as.numeric(imputeTS::na.mean(z, option = "median"))
+        z <- as.numeric(na_mean(z, option = "median"))
       }
       if (method=="random"){
-        z <- as.numeric(na.random(z))
+        z <- as.numeric(na_mean(z))
       }
       if (method=="Season.int.spline"){
-        z <- as.numeric(na.seadec(z, algorithm = "interpolation", option = "spline"))
+        z <- as.numeric(na_seadec(z, algorithm = "interpolation", option = "spline"))
       }
       if (method=="Season.int.linear"){
-        z <- as.numeric(na.seadec(z, algorithm = "interpolation", option = "linear"))
+        z <- as.numeric(na_seadec(z, algorithm = "interpolation", option = "linear"))
       }
       if (method=="Season.int.stine"){
-        z <- as.numeric(na.seadec(z, algorithm = "interpolation", option = "stine"))
+        z <- as.numeric(na_seadec(z, algorithm = "interpolation", option = "stine"))
       }
       if (method=="Season.LOCF"){
-        z <- as.numeric(na.seadec(z, algorithm = "locf", option = "locf", na.remaining = "rev"))
+        z <- as.numeric(na_seadec(z, algorithm = "locf", option = "locf", na.remaining = "rev"))
       }
       if (method=="Season.NOCB"){
-        z <- as.numeric(na.seadec(z, algorithm = "locf", option = "nocb", na.remaining = "rev"))
+        z <- as.numeric(na_seadec(z, algorithm = "locf", option = "nocb", na.remaining = "rev"))
       }
       if (method=="Season.mean"){
-        z <- as.numeric(na.seadec(z, algorithm = "mean", option = "mean"))
+        z <- as.numeric(na_seadec(z, algorithm = "mean", option = "mean"))
       }
       if (method=="Season.median"){
-        z <- as.numeric(na.seadec(z, algorithm = "mean", option = "median"))
+        z <- as.numeric(na_seadec(z, algorithm = "mean", option = "median"))
       }
       if (method=="Season.mode"){
-        z <- as.numeric(na.seadec(z, algorithm = "mean", option = "mode"))
+        z <- as.numeric(na_seadec(z, algorithm = "mean", option = "mode"))
       }
       if (method=="Season.kalman"){
-        z <- as.numeric(na.seadec(z, algorithm = "kalman"))
+        z <- as.numeric(na_seadec(z, algorithm = "kalman"))
       }
       if (method=="Season.random"){
-        z <- as.numeric(na.seadec(z, algorithm = "random"))
+        z <- as.numeric(na_seadec(z, algorithm = "random"))
       }
       if (method=="Season.MA"){
-        z <- as.numeric(na.seadec(z, algorithm = "ma"))
+        z <- as.numeric(na_seadec(z, algorithm = "ma"))
       }
       return(z)
     }),dsub_rem)
